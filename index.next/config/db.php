6710ce55803abf08ad992ff57116979e1580a9e8
elrevin <td@index-art.ru>
2015-02-09 18:19:15 +0500

    Удалил файл подключения к БД из гита

--- index.next/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=in2',
-    'username' => 'root',
-    'password' => '130283',
-    'charset' => 'utf8',
-];
