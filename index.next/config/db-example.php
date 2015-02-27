b2faad3912679f936d1987b0a8ab02abdd5564c2
elrevin <elrevin@gmail.com>
2015-02-28 02:16:55 +0500

    Добавил db-example

---
+++ index.next/config/db-example.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=BASE-NAME',
+    'username' => 'USER-NAME',
+    'password' => 'PASSWORD',
+    'charset' => 'utf8',
+];
