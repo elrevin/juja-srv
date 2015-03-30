7a165880b0ec75f1f6883b476a6a8e4221a60239
elrevin <td@index-art.ru>
2015-03-30 10:52:26 +0500

    Баг с directrequerst запросами

--- index.next/config/web.php
+++ index.next/config/web.php
@@ -87,7 +87,7 @@ $config = [
 
                 'admin/getJS/<module:[\w-]+>/<modelName:[\w-]+>/<file:[\w-\./]+>' => '<module>/adm-main/get-js-file.js',
 
-                'directrequest/<module:[\w-]+>/<controller:[\w-]+>/<action:[\w-\.]+>' => '<module>/<controller>/<action>',
+                'directrequest/<module:[\w-]+>/<controller:[\w-]+>/<action:[\w-\.]+>' => '<module>/directrequest-<controller>/<action>',
             ],
         ],
         'response' => [
