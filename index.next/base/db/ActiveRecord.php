3b93deb076affed413c9db171375357fdbf4f21c
elrevin <td@index-art.ru>
2015-03-30 10:45:16 +0500

    Поправил баг с добавлением записи в модель у которой sortable = true

--- index.next/base/db/ActiveRecord.php
+++ index.next/base/db/ActiveRecord.php
@@ -926,7 +926,7 @@ class ActiveRecord extends db\ActiveRecord
         $this->mapJson($data);
 
         if (static::$sortable) {
-            $maxSortPriority = \app\modules\catalog\models\Categories::find()->max('sort_priority');
+            $maxSortPriority = call_user_func([static::className(), 'find'])->max('sort_priority');
             $this->sort_priority = $maxSortPriority + 1;
         }
 
