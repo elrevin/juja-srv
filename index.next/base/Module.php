a2a13afdbcef16531d818b22e358eee9a145b5f1
elrevin <td@index-art.ru>
2015-07-25 11:25:52 +0500

    Сделал возможность исключить модуль из списка при создании раздела

--- index.next/base/Module.php
+++ index.next/base/Module.php
@@ -2,9 +2,14 @@
 namespace app\base;
 class Module extends \yii\base\Module
 {
+    static protected $inSiteStructure = true;
     static public function getModuleTitle()
     {
         return static::className();
     }
 
+    static public function getInSiteStructure()
+    {
+        return static::$inSiteStructure;
+    }
 }
