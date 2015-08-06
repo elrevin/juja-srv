8087b7e566e50e4d8f08fbb2462ca186c8c42514
elrevin <elrevin@gmail.com>
2015-08-06 22:34:16 +0500

    убрал суфикс и редирект с урла без слеша, на урл со слешем для AJAX запросов, а так же для всех запросов directrequest и admin

--- index.next/components/UrlManager.php
+++ index.next/components/UrlManager.php
@@ -9,15 +9,12 @@ class UrlManager extends \yii\web\UrlManager
     public function init()
     {
         // Загружаем правила
-
         foreach (\Yii::$app->modules as $name => $moduleConf) {
             $file = \Yii::getAlias("@app/modules/{$name}/urlRules.php");
             if (is_file($file)) {
                 $this->addRules(require_once($file));
             }
         }
-
-
         parent::init();
     }
 
@@ -31,14 +28,19 @@ class UrlManager extends \yii\web\UrlManager
          * Если в конце URL нет слеша, добавляем его и выполняем 301 редирект
          */
         $url = $request->getAbsoluteUrl();
-        if (preg_match('|[^/]\?|', $url)) {
-            \Yii::$app->response->statusCode = 301;
-            \Yii::$app->response->redirect(str_replace('?', '/?', $url));
-            \Yii::$app->end();
-        } elseif (strpos($url, '?') === false && $url[strlen($url) - 1] != '/') {
-            \Yii::$app->response->statusCode = 301;
-            \Yii::$app->response->redirect($url."/");
-            \Yii::$app->end();
+        $relUrl = $request->getUrl();
+        if (!preg_match("|^/?directrequest|", $relUrl) && !preg_match("|^/?admin|", $relUrl) && !$request->isAjax) {
+            if (preg_match('|[^/]\?|', $url)) {
+                \Yii::$app->response->statusCode = 301;
+                \Yii::$app->response->redirect(str_replace('?', '/?', $url));
+                \Yii::$app->end();
+            } elseif (strpos($url, '?') === false && $url[strlen($url) - 1] != '/') {
+                \Yii::$app->response->statusCode = 301;
+                \Yii::$app->response->redirect($url . "/");
+                \Yii::$app->end();
+            }
+        } else {
+            $this->suffix = '';
         }
 
         $route = parent::parseRequest($request);
