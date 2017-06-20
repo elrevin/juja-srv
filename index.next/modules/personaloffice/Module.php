bd55128f68f8f3e4b272347450d1d66460557358
elrevin <td@index-art.ru>
2017-06-20 10:32:49 +0500

    fuck, I forgot to change git repositories

--- index.next/modules/personaloffice/Module.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\modules\personaloffice;
-class Module extends \app\base\Module
-{
-    static protected $moduleTitle = 'Личный кабинет';
-    static protected $inSiteStructure = false;
-}
